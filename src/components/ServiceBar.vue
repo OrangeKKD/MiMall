542a9b2713add04c8df99644461f9b5301339784
zichen.xiang <857719037@qq.com>
2022-05-28 10:22:05 +0800

    完成底部导航栏开发

---
+++ src/components/ServiceBar.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="service">
+    <div class="container">
+      <ul>
+        <li><span class="icon-setting"></span>预约维修服务</li>
+        <li><span class="icon-7day"></span>7天无理由退货</li>
+        <li><span class="icon-15day"></span>15天免费换货</li>
+        <li><span class="icon-post"></span>满150元包邮</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ServiceBar'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/mixin.scss';
+@import '@/assets/scss/base.scss';
+.service {
+  .container {
+    padding: 33px 0;
+    ul {
+      display: flex;
+      font-size: 16px;
+      color: #666666;
+      li {
+        flex: 1;
+        text-align: center;
+        border-right: 1px solid #e5e5e5;
+        span {
+          width: 20px;
+          height: 20px;
+          vertical-align: middle;
+          margin-right: 8px;
+        }
+        .icon-setting {
+          @include bgImg(20px, 20px, '../../public/imgs/icon-setting.png');
+        }
+        .icon-7day {
+          @include bgImg(20px, 20px, '../../public/imgs/icon-7day.png');
+        }
+        .icon-15day {
+          @include bgImg(20px, 20px, '../../public/imgs/icon-15day.png');
+        }
+        .icon-post {
+          @include bgImg(20px, 20px, '../../public/imgs/icon-post.png');
+        }
+      }
+    }
+  }
+}
+</style>
